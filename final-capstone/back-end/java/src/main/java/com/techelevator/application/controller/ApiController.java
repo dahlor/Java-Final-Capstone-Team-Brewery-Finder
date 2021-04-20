4b9409fd8a5294330c3c9a387e46b2ea49da8720
Eric Dahl <dahlor@Orion.local>
2021-04-20 18:43:54 -0400

    API Controller URL tweaks for migration.

--- final-capstone/back-end/java/src/main/java/com/techelevator/application/controller/ApiController.java
+++ final-capstone/back-end/java/src/main/java/com/techelevator/application/controller/ApiController.java
@@ -45,146 +45,146 @@ public class ApiController {
 		this.reviewsDAO = reviewsDAO;
 	}
 	
-	@RequestMapping(path = "/breweries/{obdbId}", method = RequestMethod.GET)
+	@RequestMapping(path = "/breweryfinder/breweries/{obdbId}", method = RequestMethod.GET)
 	public List<Brewery> getSingleBreweryInfo(@PathVariable String obdbId) {
 	    return breweryDAO.getSingleBreweryInfo(obdbId);
 	}
 	
-	@RequestMapping(path = "/breweries/location/zip/{zipCode}", method = RequestMethod.GET)
+	@RequestMapping(path = "/breweryfinder/breweries/location/zip/{zipCode}", method = RequestMethod.GET)
 	public List<Brewery> getBreweriesByZipCode(@PathVariable String zipCode) {
 	    return breweryDAO.getBreweriesByZipCode(zipCode);
 	}
 	
-	@RequestMapping(path = "/breweries/name/{name}", method = RequestMethod.GET)
+	@RequestMapping(path = "/breweryfinder/breweries/name/{name}", method = RequestMethod.GET)
 	public List<Brewery> getBreweriesByName(@PathVariable String name) {
 	    return breweryDAO.getBreweriesByName(name);
 	}
 	
-	@RequestMapping(path = "/breweries/location/city/{city}", method = RequestMethod.GET)
+	@RequestMapping(path = "/breweryfinder/breweries/location/city/{city}", method = RequestMethod.GET)
 	public List<Brewery> getBreweriesByCity(@PathVariable String city) {
 	    return breweryDAO.getBreweriesByCity(city);
 	}
 	
-	@RequestMapping(path = "/breweries/location/state/{state}", method = RequestMethod.GET)
+	@RequestMapping(path = "/breweryfinder/breweries/location/state/{state}", method = RequestMethod.GET)
 	public List<Brewery> getBreweriesByState(@PathVariable String state) {
 	    return breweryDAO.getBreweriesByState(state);
 	}
 	
-	@RequestMapping(path = "/addBrewery", method = RequestMethod.POST)
+	@RequestMapping(path = "/breweryfinder/addBrewery", method = RequestMethod.POST)
 	public void createNewBrewery(@RequestBody Brewery formBrewery) {
 	    breweryDAO.createNewBrewery(formBrewery);
 	}
 	
-	@RequestMapping(path = "/breweries/assignBrewer", method = RequestMethod.POST)
+	@RequestMapping(path = "/breweryfinder/breweries/assignBrewer", method = RequestMethod.POST)
 	public void assignBrewer(@RequestBody Brewery formBrewery) {
 	    breweryDAO.assignBrewer(formBrewery);
 	}
 	
 	// Beer Bits
 	
-	@RequestMapping(path = "/breweries/{obdbId}/beerlist", method = RequestMethod.GET)
+	@RequestMapping(path = "/breweryfinder/breweries/{obdbId}/beerlist", method = RequestMethod.GET)
 	public List<Beer> getBeerListByBrewery(@PathVariable String obdbId) {
 	    return beerDAO.getBeerListByBrewery(obdbId);
 	}
 	
-	@RequestMapping(path = "/beers/{beerId}", method = RequestMethod.GET)
+	@RequestMapping(path = "/breweryfinder/beers/{beerId}", method = RequestMethod.GET)
 	public List<Beer> getSingleBeerInfo(@PathVariable String beerId) {
 	    return beerDAO.getSingleBeerInfo(beerId);
 	}
 
-	@RequestMapping(path = "/beers/maintenance/addBeer", method = RequestMethod.POST)
+	@RequestMapping(path = "/breweryfinder/beers/maintenance/addBeer", method = RequestMethod.POST)
 	public void addABeer(@RequestBody Beer formData) {
 	    beerDAO.addABeer(formData);
 	}
 	
-	@RequestMapping(path = "/beers/maintenance/updateBeerStatus", method = RequestMethod.POST)
+	@RequestMapping(path = "/breweryfinder/beers/maintenance/updateBeerStatus", method = RequestMethod.POST)
 	public void updateBeerStatus(@RequestBody Beer formData) {
 	    beerDAO.updateBeerStatus(formData);
 	}
 	
-	@RequestMapping(path = "/beers/maintenance/deleteBeer/{beerId}", method = RequestMethod.POST)
+	@RequestMapping(path = "/breweryfinder/beers/maintenance/deleteBeer/{beerId}", method = RequestMethod.POST)
 	public void deleteABeer(@PathVariable String beerId) {
 	    beerDAO.deleteABeer(beerId);
 	}
 	
 	// Brewery Details bit
 	
-	@RequestMapping(path = "/breweries/{obdbId}/details", method = RequestMethod.GET)
+	@RequestMapping(path = "/breweryfinder/breweries/{obdbId}/details", method = RequestMethod.GET)
 	public List<BreweryDetails> getBreweryDetailsById(@PathVariable String obdbId) {
 	    return breweryDetailsDAO.getBreweryDetailsById(obdbId);
 	}
 	
-	@RequestMapping(path = "/breweries/addDetails", method = RequestMethod.POST)
+	@RequestMapping(path = "/breweryfinder/breweries/addDetails", method = RequestMethod.POST)
 	public void addBreweryDetails(@RequestBody BreweryDetails formData) {
 	    breweryDetailsDAO.addBreweryDetails(formData);
 	}
 	
-	@RequestMapping(path = "/breweries/addDetails/history", method = RequestMethod.POST)
+	@RequestMapping(path = "/breweryfinder/breweries/addDetails/history", method = RequestMethod.POST)
 	public void updateBreweryHistory(@RequestBody BreweryDetails formData) {
 	    breweryDetailsDAO.updateBreweryHistory(formData);
 	}
 	
-	@RequestMapping(path = "/breweries/addDetails/image", method = RequestMethod.POST)
+	@RequestMapping(path = "/breweryfinder/breweries/addDetails/image", method = RequestMethod.POST)
 	public void updateBreweryImage(@RequestBody BreweryDetails formData) {
 	    breweryDetailsDAO.updateBreweryImage(formData);
 	}
 
-	@RequestMapping(path = "/breweries/addDetails/hourOpen", method = RequestMethod.POST)
+	@RequestMapping(path = "/breweryfinder/breweries/addDetails/hourOpen", method = RequestMethod.POST)
 	public void updateBreweryHourOpen(@RequestBody BreweryDetails formData) {
 	    breweryDetailsDAO.updateBreweryHourOpen(formData);
 	}
 
-	@RequestMapping(path = "/breweries/addDetails/hourClosed", method = RequestMethod.POST)
+	@RequestMapping(path = "/breweryfinder/breweries/addDetails/hourClosed", method = RequestMethod.POST)
 	public void updateBreweryHourClosed(@RequestBody BreweryDetails formData) {
 	    breweryDetailsDAO.updateBreweryHourClosed(formData);
 	}
 
-	@RequestMapping(path = "/breweries/addDetails/openSun", method = RequestMethod.POST)
+	@RequestMapping(path = "/breweryfinder/breweries/addDetails/openSun", method = RequestMethod.POST)
 	public void updateBreweryOpenSun(@RequestBody BreweryDetails formData) {
 	    breweryDetailsDAO.updateBreweryOpenSun(formData);
 	}
 
-	@RequestMapping(path = "/breweries/addDetails/openMon", method = RequestMethod.POST)
+	@RequestMapping(path = "/breweryfinder/breweries/addDetails/openMon", method = RequestMethod.POST)
 	public void updateBreweryOpenMon(@RequestBody BreweryDetails formData) {
 	    breweryDetailsDAO.updateBreweryOpenMon(formData);
 	}
 
-	@RequestMapping(path = "/breweries/addDetails/openTue", method = RequestMethod.POST)
+	@RequestMapping(path = "/breweryfinder/breweries/addDetails/openTue", method = RequestMethod.POST)
 	public void updateBreweryOpenTue(@RequestBody BreweryDetails formData) {
 	    breweryDetailsDAO.updateBreweryOpenTue(formData);
 	}
 
-	@RequestMapping(path = "/breweries/addDetails/openWed", method = RequestMethod.POST)
+	@RequestMapping(path = "/breweryfinder/breweries/addDetails/openWed", method = RequestMethod.POST)
 	public void updateBreweryOpenWed(@RequestBody BreweryDetails formData) {
 	    breweryDetailsDAO.updateBreweryOpenWed(formData);
 	}
 
-	@RequestMapping(path = "/breweries/addDetails/openThu", method = RequestMethod.POST)
+	@RequestMapping(path = "/breweryfinder/breweries/addDetails/openThu", method = RequestMethod.POST)
 	public void updateBreweryOpenThu(@RequestBody BreweryDetails formData) {
 	    breweryDetailsDAO.updateBreweryOpenThu(formData);
 	}
 
-	@RequestMapping(path = "/breweries/addDetails/openFri", method = RequestMethod.POST)
+	@RequestMapping(path = "/breweryfinder/breweries/addDetails/openFri", method = RequestMethod.POST)
 	public void updateBreweryOpenFri(BreweryDetails formData){
 	    breweryDetailsDAO.updateBreweryOpenFri(formData);
 	}
 	
-	@RequestMapping(path = "/breweries/addDetails/openSat", method = RequestMethod.POST)
+	@RequestMapping(path = "/breweryfinder/breweries/addDetails/openSat", method = RequestMethod.POST)
 	public void updateBreweryOpenSat(@RequestBody BreweryDetails formData){
 	    breweryDetailsDAO.updateBreweryOpenSat(formData);
 	}
 	
-	@RequestMapping(path = "/reviews/{beerId}", method = RequestMethod.GET)
+	@RequestMapping(path = "/breweryfinder/reviews/{beerId}", method = RequestMethod.GET)
 	public List<Reviews> getReviewsByBeerId(@PathVariable("beerId") int beerId) {
 	    return reviewsDAO.getReviewsByBeerId(beerId);
 	}
 	
-	@RequestMapping(path = "/reviews/maintenance/addReview", method = RequestMethod.POST)
+	@RequestMapping(path = "/breweryfinder/reviews/maintenance/addReview", method = RequestMethod.POST)
 	public void addReview(@RequestBody Reviews formData) {
 		reviewsDAO.addReview(formData);
 	}	
 
-	@RequestMapping(path = "/reviews/{obdbId}/averageStars", method = RequestMethod.GET)
+	@RequestMapping(path = "/breweryfinder/reviews/{obdbId}/averageStars", method = RequestMethod.GET)
 	public List<Reviews> getAverageStarsById(@PathVariable int obdbId) {
 	return reviewsDAO.getAverageStarsById(obdbId);
 	}
