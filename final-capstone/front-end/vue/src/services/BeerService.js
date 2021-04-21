7c9a9d8b17c92e1d8f58656fe5ffac32f0ddc346
Eric Dahl <dahlor@orion.local>
2021-04-21 01:06:07 -0400

    Changed services URLs to work with migration.

--- final-capstone/front-end/vue/src/services/BeerService.js
+++ final-capstone/front-end/vue/src/services/BeerService.js
@@ -7,35 +7,35 @@ const http = axios.create({
 export default {
 
       getBeersByBrewery(breweryId) {
-        return http.get(`/breweries/${breweryId}/beerlist`);
+        return http.get(`/breweryfinder/breweries/${breweryId}/beerlist`);
       },
 
       getBeerInfo(beerId) {
-        return http.get(`/beers/${beerId}`);
+        return http.get(`/breweryfinder/beers/${beerId}`);
       },
 
       addBeer(newBeer) {
-        return http.post('/beers/maintenance/addBeer', newBeer);
+        return http.post('/breweryfinder/beers/maintenance/addBeer', newBeer);
       },
 
       updateBeerStatus(newStatus) {
-        return http.post('/beers/maintenance/updateBeerStatus', newStatus);
+        return http.post('/breweryfinder/beers/maintenance/updateBeerStatus', newStatus);
       },
 
       deleteBeer(beerId) {
-          return http.post(`/beers/maintenance/deleteBeer/${beerId}`)
+          return http.post(`/breweryfinder/beers/maintenance/deleteBeer/${beerId}`)
       },
 
       getBeerReviewsById(beerId) {
-        return http.get(`/reviews/${beerId}`)
+        return http.get(`/breweryfinder/reviews/${beerId}`)
     },
     
       addBeerReview(review) {
-      return http.post('/reviews/maintenance/addReview', review)
+      return http.post('/breweryfinder/reviews/maintenance/addReview', review)
   },
 
       getAverageStarsById(beerId) {
-        return http.get(`/reviews/${beerId}/averageStars`)
+        return http.get(`/breweryfinder/reviews/${beerId}/averageStars`)
     }
 
   }
--- final-capstone/front-end/vue/src/services/BreweryServices.js
+++ final-capstone/front-end/vue/src/services/BreweryServices.js
@@ -7,86 +7,86 @@ const http = axios.create({
 export default {
 
       getSingleBreweryInfo(obdbId){
-        return http.get(`/breweries/${obdbId}`)
+        return http.get(`/breweryfinder/breweries/${obdbId}`)
       },
 
       getBreweriesByName(name) {
-        return http.get(`/breweries/name/${name}`);
+        return http.get(`/breweryfinder/breweries/name/${name}`);
       },
 
       getBreweriesByCity(city) {
-        return http.get(`/breweries/location/city/${city}`);
+        return http.get(`/breweryfinder/breweries/location/city/${city}`);
       },
 
       getBreweriesByState(state) {
-        return http.get(`/breweries/location/state/${state}`);
+        return http.get(`/breweryfinder/breweries/location/state/${state}`);
       },
 
       getBreweriesByZipCode(zipCode) {
-        return http.get(`/breweries/location/zip/${zipCode}`);
+        return http.get(`/breweryfinder/breweries/location/zip/${zipCode}`);
       },
 
       addBrewery(newBrewery) {
-        return http.post('/addBrewery', newBrewery);
+        return http.post('/breweryfinder/addBrewery', newBrewery);
       },
 
       getBreweryDetailsById(obdbId){
-        return http.get(`/breweries/${obdbId}/details`)
+        return http.get(`/breweryfinder/breweries/${obdbId}/details`)
       },
 
       addBreweryDetails(addDetails) {
-        return http.post('/breweries/addDetails', addDetails);
+        return http.post('/breweryfinder/breweries/addDetails', addDetails);
       },
 
 
       
       assignBrewer(updateDetails) {
-        return http.post('/breweries/assignBrewer', updateDetails);
+        return http.post('/breweryfinder/breweries/assignBrewer', updateDetails);
       },
 
 
 
       updateBreweryHistory(updateDetails) {
-        return http.post('/breweries/addDetails/history', updateDetails);
+        return http.post('/breweryfinder/breweries/addDetails/history', updateDetails);
       },
       
       updateBreweryImage(updateDetails) {
-        return http.post('/breweries/addDetails/image', updateDetails);
+        return http.post('/breweryfinder/breweries/addDetails/image', updateDetails);
       },
 
       updateBreweryHourOpen(updateDetails) {
-        return http.post('/breweries/addDetails/hourOpen', updateDetails);
+        return http.post('/breweryfinder/breweries/addDetails/hourOpen', updateDetails);
       },
 
       updateBreweryHourClosed(updateDetails) {
-        return http.post('/breweries/addDetails/hourClosed', updateDetails);
+        return http.post('/breweryfinder/breweries/addDetails/hourClosed', updateDetails);
       },
 
       updateBreweryOpenSun(updateDetails) {
-        return http.post('/breweries/addDetails/openSun', updateDetails);
+        return http.post('/breweryfinder/breweries/addDetails/openSun', updateDetails);
       },
 
       updateBreweryOpenMon(updateDetails) {
-        return http.post('/breweries/addDetails/openMon', updateDetails);
+        return http.post('/breweryfinder/breweries/addDetails/openMon', updateDetails);
       },
 
       updateBreweryOpenTue(updateDetails) {
-        return http.post('/breweries/addDetails/openTue', updateDetails);
+        return http.post('/breweryfinder/breweries/addDetails/openTue', updateDetails);
       },
 
       updateBreweryOpenWed(updateDetails) {
-        return http.post('/breweries/addDetails/openWed', updateDetails);
+        return http.post('/breweryfinder/breweries/addDetails/openWed', updateDetails);
       },
 
       updateBreweryOpenThu(updateDetails) {
-        return http.post('/breweries/addDetails/openThu', updateDetails);
+        return http.post('/breweryfinder/breweries/addDetails/openThu', updateDetails);
       },
 
       updateBreweryOpenFri(updateDetails) {
-        return http.post('/breweries/addDetails/openFri', updateDetails);
+        return http.post('/breweryfinder/breweries/addDetails/openFri', updateDetails);
       },
 
       updateBreweryOpenSat(updateDetails) {
-        return http.post('/breweries/addDetails/openSat', updateDetails);
+        return http.post('/breweryfinder/breweries/addDetails/openSat', updateDetails);
       },
   }
